aafd604f05c8bf15119ff1c8b88301a230e20003
Dmitry Fedyuk <dfediuk@gmail.com>
2019-07-03 19:01:16 +0600

    0.0.2

---
+++ view/frontend/web/main.js
@@ -0,0 +1,37 @@
+// 2019-07-03
+define(['jquery'], function($) {return (
+/**
+ * 2019-06-02
+ * @param {Object} c
+ * @param {HTMLDivElement} e
+ */
+function(c, e) {
+	var $e = $(e);
+	$('button', $e).click(function() {
+		var days = 14; //number of days until they must go through the age checker again.
+		var date = new Date;
+		date.setTime (date.getTime() + days * 24 * 60 * 60 * 1000);
+		var expires = "; expires=" + date.toGMTString ();
+		document.cookie = 'isAnAdult=true;' + expires + "; path=/";
+		location.reload();
+	});
+	(function() {
+		var readCookie = function() {
+			var nameEQ = 'isAnAdult=';
+			var ca = document.cookie.split(';');
+			for (var i = 0; i < ca.length; i++) {
+				var c = ca[i];
+				while (c.charAt (0) == ' ') {
+					c = c.substring (1, c.length);
+				}
+				if (c.indexOf (nameEQ) == 0) {
+					return c.substring (nameEQ.length, c.length);
+				}
+			}
+			return null;
+		};
+		if (readCookie()) {
+			$e.hide();
+		}
+	})();
+});});
